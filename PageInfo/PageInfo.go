b57d4ae89dddd4d3ed55a13d007f4b8b034defe9
HackeZ <767110505@qq.com>
2016-07-05 16:58:48 +0800

    add GtePageInfo

---
+++ PageInfo/PageInfo.go
@@ -0,0 +1,70 @@
+package PageInfo
+
+import (
+	"fmt"
+	"getAcFunPage/utils"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// Author: HackerZ
+// Time  : 2016-7-5 11:47
+
+// PageInfo Some Infomation of the Page.
+type PageInfo struct {
+	PageID    string
+	Onlooker  int64
+	Comments  int64
+	Banana    int64
+	Published string
+}
+
+var (
+	acfunRoot   = "http://www.acfun.tv"
+	ptnPageInfo = regexp.MustCompile(`<span class="pts">([0-9]+)</span><span>围观</span>&nbsp;·&nbsp;&nbsp;<span class="pts pointer">([0-9]+)</span><span class="pinglun">评论</span>&nbsp;·&nbsp;&nbsp;<span class="pts">([0-9]+)</span><span>香蕉</span>&nbsp;/&nbsp;&nbsp;<span class="time">发布于 ([0-9]+年 [0-9]+月[0-9]+日 [0-9]+:[0-9]+)</span>`)
+)
+
+// GetPageInfo Return Page Info which pageID.
+func GetPageInfo(pageID string) PageInfo {
+	raw, statusCode := getPageInfo(acfunRoot + pageID)
+	if statusCode != "200 OK" {
+		fmt.Printf("Get %s PageInfo Error.\n", pageID)
+	}
+
+	pageInfo := findPageInfo(pageID, raw)
+	return pageInfo
+}
+
+func getPageInfo(url string) (content, statusCode string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		statusCode = "-1"
+		fmt.Println("Get PageInfo Error -->", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	bys, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		statusCode = "-2"
+		fmt.Println("Get PageInfo Error -->", err.Error())
+		return
+	}
+	statusCode = resp.Status
+	content = string(bys)
+	return
+}
+
+func findPageInfo(pageID, content string) PageInfo {
+	matches := ptnPageInfo.FindStringSubmatch(content)
+
+	// fmt.Println("PageInfo", matches)
+	return PageInfo{
+		pageID,
+		utils.StrToInt64(matches[1]),
+		utils.StrToInt64(matches[2]),
+		utils.StrToInt64(matches[3]),
+		matches[4]}
+
+}
---
+++ utils/TypeConver.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"strconv"
+)
+
+// Author: HackerZ
+// Time  : 2016-7-5 14:32
+
+// StrToInt64 Change String to Int64
+func StrToInt64(s string) (i64 int64) {
+	i64, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
