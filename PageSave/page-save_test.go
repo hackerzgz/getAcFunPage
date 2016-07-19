ff1a551be569a2d686d06e5d92be4bbc8b17a59f
HackeZ <767110505@qq.com>
2016-07-19 21:26:16 +0800

    fix test wrong

--- PageSave/page-save_test.go
+++ PageSave/page-save_test.go
@@ -9,8 +9,8 @@ func TestHset(t *testing.T) {
 	fmt.Println("=== Testing HSET Start ===")
 	n, err := HMset("ac2905884", "TESTING HSET TITLE", "TESTING HSET URL", 35844, 1454, 2)
 
-	if err != nil || n != "5" {
-		t.Errorf("error --> %s || size_length --> %s", err.Error(), n)
+	if err != nil || n == "-1" {
+		t.Errorf("error --> %s --> %s", err.Error())
 	}
 
 	fmt.Println("=== Testing HSET Done ===")
