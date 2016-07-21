831a4c52f804be6da2b18413ab24066a4948578f
HackeZ <767110505@qq.com>
2016-07-21 22:28:21 +0800

    add annotation

--- PageSave/page-to-json.go
+++ PageSave/page-to-json.go
@@ -6,6 +6,10 @@ import (
 	"github.com/emilsjolander/goson"
 )
 
+/* Page2JSON use goson to reflex IndexItem to JSON
+ * @param 	indexItem
+ * @return 	result, err
+ */
 func Page2JSON(indexItem IndexItem) (string, error) {
 	itemJSON, err := goson.Render("../ac_page", goson.Args{"IndexItem": indexItem})
 
@@ -16,6 +20,10 @@ func Page2JSON(indexItem IndexItem) (string, error) {
 	return string(itemJSON), nil
 }
 
+/* Pages2JSON use goson to reflex []IndexItem to JSON
+ * @param 	itemList
+ * @return 	result, err
+ */
 func Pages2JSON(indexList []IndexItem) (string, error) {
 	itemJSON, err := goson.Render("../ac_pages", goson.Args{"IndexItem": indexList})
 
